c88aa936507e3b9a9871a0a2332fd0eda4b99599
davide-vigano-tas <davide.vigano@tasgroup.eu>
2025-01-27 14:57:35 +0100

    Fix Cliente and Add new featuring

--- src/main/java/eu/tasgroup/gestione/web/bean/ClienteFacadeBean.java
+++ src/main/java/eu/tasgroup/gestione/web/bean/ClienteFacadeBean.java
@@ -1,10 +1,14 @@
 package eu.tasgroup.gestione.web.bean;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.naming.NamingException;
@@ -13,7 +17,6 @@ import eu.tasgroup.gestione.architetture.dao.DAOException;
 import eu.tasgroup.gestione.businesscomponent.facade.ClienteFacade;
 import eu.tasgroup.gestione.businesscomponent.model.Payment;
 import eu.tasgroup.gestione.businesscomponent.model.Project;
-import eu.tasgroup.gestione.businesscomponent.model.Role;
 import eu.tasgroup.gestione.businesscomponent.model.User;
 
 @Named
@@ -27,6 +30,11 @@ public class ClienteFacadeBean implements Serializable {
 	private Project project;
 	private Payment payment;
 	private List<Project> projects;
+	private List<Payment> payments;
+	private List<Payment> paymentsUser;
+
+	private boolean success;
+	private boolean error;
 
 	@Inject
 	private UserSessionBean userSessionBean;
@@ -38,72 +46,14 @@ public class ClienteFacadeBean implements Serializable {
 	@PostConstruct
 	public void init() {
 		try {
-			this.user = getByUsername(userSessionBean.getUsername());
-			this.projects = getProjectsByCliente(user);
+			this.user = cf.getByUsername(userSessionBean.getUsername());
+			this.projects = cf.getProjectsByCliente(user);
 			this.payment = new Payment();
+			this.paymentsUser = Arrays.asList(cf.getPaymentByCliente(user));
+
 		} catch (DAOException | NamingException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public User createOrUpdateCliente(User user) throws DAOException, NamingException {
-		return cf.createOrUpdateCliente(user);
-	}
-	public String createCliente(User user) throws DAOException, NamingException {
-		cf.createOrUpdateCliente(user);
-		return "login?i=2&faces-redirect=true";
-	}
-
-	public User getById(long id) throws DAOException, NamingException {
-		return cf.getById(id);
-	}
-
-	public User getByUsername(String username) throws DAOException, NamingException {
-		return cf.getByUsername(username);
-	}
-
-	public User getByEmail(String email) throws DAOException, NamingException {
-		return cf.getByEmail(email);
-	}
-
-	public int getPercentualeCompletamentoProjectID(long id) throws DAOException, NamingException {
-		return cf.getPercentualeCompletamentoProjectID(id);
-	}
-
-	public Project getProjectById(long id) throws DAOException, NamingException {
-		return cf.getProjectById(id);
-	}
-
-	public List<Project> getProjectsByCliente(User user) throws DAOException, NamingException {
-		return cf.getProjectsByCliente(user);
-	}
-
-	public void createPayment(Payment payment) throws DAOException, NamingException {
-		cf.createPayment(payment);
-	}
-
-	public Payment getPaymentById(long id) throws DAOException, NamingException {
-		return cf.getPaymentById(id);
-	}
-
-	public Payment[] getPaymentByProject(Project project) throws DAOException, NamingException {
-		return cf.getPaymentByProject(project);
-	}
-
-	public Payment[] getPaymentByCliente(User cliente) throws DAOException, NamingException {
-		return cf.getPaymentByCliente(cliente);
-	}
-
-	public Role[] getRolesById(long id) throws DAOException, NamingException {
-		return cf.getRolesById(id);
-	}
-
-	public double getTotalPaymentsByProjectId(long id) throws DAOException, NamingException {
-		return cf.getTotalPaymentsByProjectId(id);
-	}
-
-	public void saveLogMessage(String username, String operazione) throws DAOException, NamingException {
-		cf.saveLogMessage(username, operazione);
 	}
 
 	public User getUser() {
@@ -137,16 +87,111 @@ public class ClienteFacadeBean implements Serializable {
 	public void setProjects(List<Project> projects) throws DAOException, NamingException {
 		this.projects = projects;
 	}
-	
-	public void confirmPayment() throws DAOException, NamingException {
-		cf.createPayment(payment);
+
+	public List<Payment> getPayments() {
+		return payments;
+	}
+
+	public void setPayments(List<Payment> payments) {
+		this.payments = payments;
+	}
+
+	public List<Payment> getPaymentsUser() {
+		return paymentsUser;
+	}
+
+	public void setPaymentsUser(List<Payment> paymentsUser) {
+		this.paymentsUser = paymentsUser;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public boolean isError() {
+		return error;
+	}
+
+	public void setError(boolean error) {
+		this.error = error;
+	}
+
+	public User createOrUpdateCliente(User user) throws DAOException, NamingException {
+		return cf.createOrUpdateCliente(user);
+	}
+
+	public String createCliente(User user) throws DAOException, NamingException {
+		cf.createOrUpdateCliente(user);
+		return "login?i=2&faces-redirect=true";
+	}
+
+	public void confirmPayment() {
+		try {
+			double maxPagamento = cf.getProjectById(payment.getIdProgetto()).getCostoProgetto()
+					- cf.getTotalPaymentsByProjectId(payment.getIdProgetto());
+
+			if (maxPagamento <= 0 || maxPagamento - payment.getCifra() < 0) {
+				FacesContext.getCurrentInstance().getExternalContext().redirect(
+						"/" + UserSessionBean.getServletContextName() + "/cliente/cliente-home.xhtml?error=true");
+				return;
+			}
+
+			cf.createPayment(payment);
+			FacesContext.getCurrentInstance().getExternalContext().redirect(
+					"/" + UserSessionBean.getServletContextName() + "/cliente/cliente-home.xhtml?success=true");
+		} catch (DAOException | NamingException e) {
+			try {
+				FacesContext.getCurrentInstance().getExternalContext().redirect(
+						"/" + UserSessionBean.getServletContextName() + "/cliente/cliente-home.xhtml?error=true");
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showPaymentHistory(long projectID) {
+		try {
+			// Recupera i dettagli del progetto e i pagamenti
+			Project p = cf.getProjectById(projectID);
+			payments = Arrays.asList(cf.getPaymentByProject(p));
+			System.out.println("Pagamenti caricati: " + payments);
+
+			// Redirezione alla pagina lista-pagamenti
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("/" + UserSessionBean.getServletContextName() + "/cliente/lista-pagamenti.xhtml");
+		} catch (DAOException | NamingException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public double calcolaDaVersare(long projectID) {
+		try {
+			return cf.getProjectById(projectID).getCostoProgetto() - cf.getTotalPaymentsByProjectId(projectID);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0.0;
+		}
 	}
 	
-	public void preparePaymentModal(Project project) {
-	    this.payment = new Payment(); // O un'istanza preesistente
-	    this.payment.setIdProgetto(project.getId());
-	    this.payment.setCifra(project.getCostoProgetto());
-	    this.project = project; // Per nome e altri dettagli
+	public void displayMessage() {
+	    FacesContext context = FacesContext.getCurrentInstance();
+	    if (success) {
+	    	success = false;
+	        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, 
+	            "Pagamento Confermato", "Il pagamento è stato registrato con successo."));
+	    }
+	    if (error) {
+	    	error = false;
+	        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+	            "Pagamento Rifiutato", "Il pagamento supera il limite massimo o non è consentito."));
+	    }
 	}
 
 }
